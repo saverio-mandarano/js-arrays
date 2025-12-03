bdb9b409254650e6b7523835322e3e14959359f1
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-03 18:28:50 +0100

    3. Rimuovi 'Ed' dall'array teachers

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -8,19 +8,35 @@ const teachers = [
   'Luca'
 ]; // NON MODIFICARE QUESTA VARIABILE
 
+
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.reverse();
+const reversedTeachers = teachers.slice().reverse();
 for (let i=0; i<teachers.length; i++){
-
   console.log("Array invertito: " + reversedTeachers[i]);
 }
+
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 const longNames = null;
 
+
 // 3. Rimuovi 'Ed' dall'array teachers
+const indexOfEd = teachers.indexOf(`Ed`)
+console.log("Indice di Ed: " + indexOfEd);
+
+if (indexOfEd !== -1) {      //check - Ed esiste ?    
+  teachers.splice(indexOfEd, 1); 
+}
+else {
+  console.log(`Ed non trovato nell'array.`);
+}
+
+console.log("Array dopo la rimozione di Ed: ");
+for (let i=0; i<teachers.length; i++){
+  console.log("Array indice: " + i + " valore: " + teachers[i]);
+}
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
