a8dbb8a0626a878ff3da8f857aa5633036a59ad8
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-03 15:58:52 +0100

    2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -8,11 +8,17 @@ const teachers = [
   'Luca'
 ]; // NON MODIFICARE QUESTA VARIABILE
 
-// 1. Utilizzando un indice sull'array teachers, cambia il valore della
-// variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
+// 1. Utilizzando un indice sull'array teachers, cambia il valore della 
+// variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array.
 const fourthTeacher = teachers [3];
+//console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+teachers.splice(4, 1, 'Patrick');
+
+// for (let i=0; i<teachers.length; i++){
+//   console.log(teachers[i]);
+// }
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = null;
